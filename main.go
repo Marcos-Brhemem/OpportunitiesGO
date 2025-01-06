0f5589054393a818bee0924651b3c954d15aa6e8
Marc-brm <bremen.mcn@gmail.com>
2025-01-06 16:48:26 -0300

    feat: add custom logger

--- main.go
+++ main.go
@@ -1,7 +1,27 @@
 package main
 
-import "github.com/Marcos-Brhemem/OpportunitiesGO/router"
+import (
+	"github.com/Marcos-Brhemem/OpportunitiesGO/config"
+	"github.com/Marcos-Brhemem/OpportunitiesGO/router"
+)
+
+var (
+	looger *config.Logger
+)
 
 func main() {
+
+	looger = config.GetLogger("main")
+
+	// Initialize Configs
+
+	err := config.Init()
+
+	if err != nil {
+		looger.Errorf("config initialization error: %v", err)
+		return
+	}
+
+	// Initialize Router
 	router.Initialize()
 }
