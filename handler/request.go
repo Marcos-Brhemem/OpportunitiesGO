429cd459b8593277f0c629864799fc02572f7210
Marc-brm <bremen.mcn@gmail.com>
2025-01-09 16:33:30 -0300

    feat: add update handler logic

--- handler/request.go
+++ handler/request.go
@@ -41,3 +41,23 @@ func (r *CreatingOpeningRequest) Validate() error {
 
 	return nil
 }
+
+// update opening
+
+type UpdateOpeningRequest struct {
+	Role     string `json:"role"`
+	Company  string `json:"company"`
+	Location string `json:"location"`
+	Remote   *bool  `json:"remote"`
+	Link     string `json:"link"`
+	Salary   int64  `json:"salary"`
+}
+
+func (r *UpdateOpeningRequest) Validate() error {
+	// if any field is provided, validate is truthy
+	if r.Role != "" || r.Company != "" || r.Location != "" || r.Link != "" || r.Remote != nil || r.Salary > 0 {
+		return nil
+	}
+	// if none of the fields are provided, return false
+	return fmt.Errorf("at least one valid field must be provided")
+}
--- handler/updateOpening.go
+++ handler/updateOpening.go
@@ -1,11 +1,63 @@
 package handler
 
 import (
+	"fmt"
 	"net/http"
 
+	"github.com/Marcos-Brhemem/OpportunitiesGO/schemas"
 	"github.com/gin-gonic/gin"
 )
 
 func UpdateOpeningHandler(ctx *gin.Context) {
-	ctx.JSON(http.StatusOK, gin.H{"message": "PUT Opening"})
+	request := UpdateOpeningRequest{}
+
+	ctx.BindJSON(&request)
+
+	if err := request.Validate(); err != nil {
+		looger.Errorf("validation error: %v", err.Error())
+		SendError(ctx, http.StatusBadRequest, err.Error())
+		return
+	}
+
+	id := ctx.Query("id")
+	if id == "" {
+		SendError(ctx, http.StatusBadRequest, errParamIsRequired("id", "queryParameter").Error())
+		return
+	}
+	opening := schemas.Opening{}
+	if err := db.First(&opening, id).Error; err != nil {
+		SendError(ctx, http.StatusNotFound, "opening not found")
+		return
+	}
+	// Update opening
+	if request.Role != "" {
+		opening.Role = request.Role
+	}
+
+	if request.Company != "" {
+		opening.Company = request.Company
+	}
+
+	if request.Location != "" {
+		opening.Location = request.Location
+	}
+
+	if request.Remote != nil {
+		opening.Remote = *request.Remote
+	}
+
+	if request.Link != "" {
+		opening.Link = request.Link
+	}
+
+	if request.Salary > 0 {
+		opening.Salary = fmt.Sprintf("%d", request.Salary)
+	}
+	// Save opening
+	if err := db.Save(&opening).Error; err != nil {
+		looger.Errorf("error updating opening: %v", err.Error())
+		SendError(ctx, http.StatusInternalServerError, "error updating opening")
+		return
+	}
+	SendSucess(ctx, "update-opening success", opening)
 }
